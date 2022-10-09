b1483f28c6c8b50564e39ecee6c289b90973c786
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-09 18:37:57 +0100

    I instantiated an object

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,20 @@
 public class Person {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1. name = "Chinwe";
+        person1. age = 30;
+
+        Person person2 = new Person();
+        person2.name = "Jane";
+        person2.age = 42;
+
+        System.out.println("I know " + person1.name + " age");
+        System.out.println(person1.name + " is "+ person1.age);
+
+    }
 }
