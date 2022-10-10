5c85911ef9cd043534ebdd90acf251d82a3b4549
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-10 13:26:24 +0100

    i added the setAge

--- src/DataTest.java
+++ src/DataTest.java
@@ -6,8 +6,17 @@ public class DataTest {
 
         Data myData = new Data();
 
-        System.out.println("What is your name");
+        System.out.println("What is your name? ");
         String myName = input.nextLine();
         myData.setName(myName);
+
+        System.out.println("Welcome," + myData.getName());
+        System.out.println();
+
+        System.out.println("Enter your grade: ");
+        int myGrade = input.nextInt();
+        myData.setGrade(myGrade);
+
+        System.out.println("Your grade is: " + myData.getGrade());
     }
 }
