b69c9d9e5485043cb39214b451d68330ca4b76f8
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-10 10:10:59 +0100

    I assighned a person.

--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,7 @@ public class Person {
 
     public static void main(String[] args) {
 
+        // Just like humans(Person in this case). Everybody is a human but, with different names and information.
         Person person1 = new Person();
         person1. name = "Chinwe";
         person1. age = 30;
@@ -13,8 +14,10 @@ public class Person {
         person2.name = "Jane";
         person2.age = 42;
 
+
         System.out.println("I know " + person1.name + " age");
         System.out.println(person1.name + " is "+ person1.age);
 
+        System.out.println("Just assigned a person to africa " + person2);
     }
 }
