06489b59a4d916fee2bc06b94f497b54e9aaa47f
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-10 13:26:57 +0100

    i added the setAge

---
+++ src/Data.java
@@ -0,0 +1,21 @@
+public class Data {
+
+    private String name;
+    private int grade;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+}
