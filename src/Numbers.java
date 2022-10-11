06fe40d75a7664144589811e7bff7eaf1b4ff7b9
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-11 18:09:01 +0100

    Initialized an array.

---
+++ src/Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+
+    public static void main(String[] args) {
+
+        int [] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        System.out.printf("%4s%8s%n", "Index" , "Values");
+
+
+        for (int counter = 1; counter < array.length; counter++)
+            System.out.printf("%3d%8d%n" , counter , array[counter] );
+    }
+}
