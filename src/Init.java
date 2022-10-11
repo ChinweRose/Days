678c36aecc681d76f88dd4adafb4ea6ceaa77bd0
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-11 16:59:56 +0100

    I created an array

---
+++ src/Init.java
@@ -0,0 +1,11 @@
+public class Init {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+
+        System.out.printf("%5s%8s%n", "index" , "Value");
+
+        for(int counter = 0 ; counter < array.length ; counter++)
+            System.out.printf("%5d%8d%n", counter , array[counter] );
+    }
+}
